845fa9fc0245818b43104869700d0b1db916eadc
Mark Bailey <mark@madeinkatana.com>
2020-07-14 15:34:24 +0930

    Write declaration tests for transform

--- tests/declarations.test.js
+++ tests/declarations.test.js
@@ -1400,6 +1400,35 @@ const tests = {
 		"transition-margin-right-4s,color-1s": "transition: margin-right 4s, color 1s",
 		"transition-all-0.5s-ease-out": "transition: all 0.5s ease-out",
 	},
+	transform: {
+		"transform-none": "transform: none",
+		"transform-matrix(1.0,2,3.0,4.0,5,6)": "transform: matrix(1, 2, 3, 4, 5, 6)",
+		"transform-matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1)":
+			"transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)",
+		"transform-perspective(17px)": "transform: perspective(17px)",
+		"transform-rotate(0.5turn)": "transform: rotate(0.5turn)",
+		"transform-rotate3d(1,2.0,3.0,10deg)": "transform: rotate3d(1, 2, 3, 10deg)",
+		"transform-rotateX(10deg)": "transform: rotateX(10deg)",
+		"transform-rotateY(10deg)": "transform: rotateY(10deg)",
+		"transform-rotateZ(10deg)": "transform: rotateZ(10deg)",
+		"transform-translate(12px,50%)": "transform: translate(12px, 50%)",
+		"transform-translate3d(12px,50%,3em)": "transform: translate3d(12px, 50%, 3em)",
+		"transform-translateX(2em)": "transform: translateX(2em)",
+		"transform-translateY(3in)": "transform: translateY(3in)",
+		"transform-translateZ(2px)": "transform: translateZ(2px)",
+		"transform-scale(2,0.5)": "transform: scale(2, 0.5)",
+		"transform-scale3d(2.5,1.2,0.3)": "transform: scale3d(2.5, 1.2, 0.3)",
+		"transform-scaleX(2)": "transform: scaleX(2)",
+		"transform-scaleY(0.5)": "transform: scaleY(0.5)",
+		"transform-scaleZ(0.3)": "transform: scaleZ(0.3)",
+		"transform-skew(30deg,20deg)": "transform: skew(30deg, 20deg)",
+		"transform-skewX(30deg)": "transform: skewX(30deg)",
+		"transform-skewY(1.07rad)": "transform: skewY(1.07rad)",
+		"transform-translateX(10px)-rotate(10deg)-translateY(5px)":
+			"transform: translateX(10px) rotate(10deg) translateY(5px)",
+		"transform-perspective(500px)-translate(10px,0,20px)-rotateY(3deg)":
+			"transform: perspective(500px) translate(10px, 0, 20px) rotateY(3deg)",
+	},
 };
 
 for (const [property, propertyTests] of Object.entries(tests)) {
